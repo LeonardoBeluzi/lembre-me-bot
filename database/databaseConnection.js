6c02db065cd71b28bafb02573630e040aa5b064c
Leonardo Beluzi Loureiro <leonardobeluzi@gmail.com>
2024-08-29 15:15:23 -0300

    change database to mysql

--- database/databaseConnection.js
+++ database/databaseConnection.js
@@ -1,8 +1,12 @@
 const { Sequelize } = require("sequelize");
 
 module.exports = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database/db.sqlite",
+  dialect: "mysql",
+  database: process.env.MYSQL_DATABASE_NAME,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   logging: false,
   define: {
     timestamps: false,
